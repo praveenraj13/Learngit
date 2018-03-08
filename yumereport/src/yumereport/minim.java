d8422f742af38cd7cf94184ac512014c504e0719
RaviTeja <RaviTeja@RaviTeja-PC.jkhostel>
2018-03-08 13:37:15 +0530

    test

--- yumereport/src/yumereport/minim.java
+++ yumereport/src/yumereport/minim.java
@@ -1,5 +1,15 @@
 package yumereport;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class minim {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver=new  FirefoxDriver();
+	}
 
 }
